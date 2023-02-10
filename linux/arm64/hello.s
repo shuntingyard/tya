f8f169c90740117b40b52e94b7b94af0230f4d6e
Tobias Frei <shuntingyard@gmail.com>
2023-02-10 11:24:38 +0100

    added arm64 hello world and copycat Makefile

---
+++ linux/arm64/hello.s
@@ -0,0 +1,24 @@
+/*
+    Writes "Hello, World" to stdout using only system calls. Written for Arch64 Linux.
+
+    syscalls cf.
+        https://github.com/torvalds/linux/blob/v4.17/include/uapi/asm-generic/unistd.h
+*/
+	.global _start
+
+        .text
+
+_start:
+        mov    x8, 64           // syscall for write
+        mov    x2, hello_len
+        adr    x1, hello_txt
+        mov    x0, 1            // for stdout
+        svc    0
+
+        mov    x8, 93           // syscall for exit
+        svc    0
+
+        .data
+
+hello_txt: .ascii "Hello, World!\n"
+hello_len = . - hello_txt
